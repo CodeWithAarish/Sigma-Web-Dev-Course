a9acc846069c21455dc8177aca4c7c7d02b75da5
CodeWithAarish <mohammedaarish240@gmail.com>
2023-11-21 21:10:39 +0530

    Tutorial 55

---
+++ Tutorial 55/index.js
@@ -0,0 +1,40 @@
+console.log("Hey this is tutorila 55")
+
+var a = 5;
+a = a+1
+let b = 6;
+let c = "Aarish"
+let _a = "Harry"
+// var 55s = "Rohan" // Not allowed
+
+// console.log (a + b)
+// console.log(typeof a, typeof b, typeof c)
+{
+    var a = 12
+    console.log(a)
+}
+
+// const a1 = 6
+// a1 = a1+1 // Not allowed
+
+
+let x = "Aarish Bhai"
+let y = 22
+let z = 3.45
+const p = true
+let q = undefined
+let r = null
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    "name": "Aarish",
+    "job code": 465,
+    "is_handsome": true
+}
+console.log(o)
+o.salaries = "100cores"
+console.log(o)
+o.salaries = "500cores"
+console.log(o)
