ae674e6455e28d536d8aaff203040f923b2cc731
CodeWithAarish <mohammedaarish240@gmail.com>
2023-11-26 18:49:48 +0530

    Tutorial 57

---
+++ Tutorial 57/index.js
@@ -0,0 +1,49 @@
+console.log("I am a tutorial of javascript");
+
+let a = 1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+
+/*
+
+// For Loop
+for (let i = 0; i < 100; i++) {
+    console.log(a + i);
+}
+
+// For-In Loop
+
+let obj = {
+    name: "Aarish",
+    role: "Programmer",
+    company: "CodeWithAarish AI"
+}
+
+for (const key in obj) {
+    console.log(obj[key])
+}
+
+// For-Of Loop
+
+for (const char of "Aarish") {
+    console.log(char)
+}
+
+// While Loop
+
+let i = 0;
+while (i<6) {
+    console.log(i)
+    i++;
+}
+
+// Do-While Loop
+
+let i = 0
+do {
+    console.log(i)
+    i++;
+} while (i<6);
+
+*/
