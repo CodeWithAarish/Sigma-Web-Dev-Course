60750967b58e36a8d6a202be006979ced5c30d99
CodeWithAarish <mohammedaarish240@gmail.com>
2023-12-12 17:06:45 +0530

    Tutorial 67

---
+++ Tutorial 67/index.js
@@ -0,0 +1,9 @@
+console.log("Hello world")
+
+let a = document.body.firstElementChild 
+let b = document.body.firstElementChild.childNodes 
+let c = document.body.firstElementChild.children
+
+console.log(a)
+console.log(b)
+console.log(c)
