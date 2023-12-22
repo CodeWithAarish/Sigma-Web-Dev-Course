4bc85f4df1096b1885b9240463a38dcabf8bee12
CodeWithAarish <mohammedaarish240@gmail.com>
2023-12-22 19:09:11 +0530

    Tutorial 71

---
+++ Tutorial 71/index.js
@@ -0,0 +1,25 @@
+let a = document.querySelector(".box")
+let b = document.querySelector(".box").innerHTML
+let c = document.querySelector(".box").outerHTML
+let d = document.querySelector(".box").innerText
+let e = document.querySelector(".box").outerText
+let f = document.querySelector(".container")
+let g = document.querySelector(".container").innerHTML
+let h = document.querySelector(".container").outerHTML
+let i = document.querySelector(".container").innerText
+let j = document.querySelector(".container").outerText
+let k = document.querySelector(".container").tagName
+let l = document.querySelector(".container").nodeName
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(i)
+// console.log(j)
+// console.log(k)
+// console.log(l)
